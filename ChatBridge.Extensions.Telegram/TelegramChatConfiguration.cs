ffdd4d42d8b428eda22f70ab732c103b58034387
Order <43047529+Orrder@users.noreply.github.com>
2021-11-25 08:58:50 +0300

    Create TelegramChatConfiguration.cs

---
+++ ChatBridge.Extensions.Telegram/TelegramChatConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ChatBridge.Extensions.Telegram
+{
+    /// <summary>
+    /// Configuration for Vk Chat
+    /// </summary>
+    public class TelegramChatConfiguration
+    {
+        /// <summary>
+        /// Id of Chat
+        /// </summary>
+        [JsonPropertyName("chatId")]
+        public int? ChatId { get; set; }
+
+        /// <summary>
+        /// Access token
+        /// </summary>
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Types of <seealso cref="BridgeMessageContent"/> to ignore
+        /// </summary>
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [JsonPropertyName("ignoreMessageTypes")]
+        public IEnumerable<BridgeMessageContentType> IgnoreMessageTypes { get; set; }
+    }
+}
