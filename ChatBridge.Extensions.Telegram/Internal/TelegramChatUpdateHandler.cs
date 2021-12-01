ddcaaec6e581794b575d0352f1646a554cc57fe7
Order <43047529+Orrder@users.noreply.github.com>
2021-12-01 12:44:09 +0300

    Made it work without file

--- ChatBridge.Extensions.Telegram/Internal/TelegramChatUpdateHandler.cs
+++ ChatBridge.Extensions.Telegram/Internal/TelegramChatUpdateHandler.cs
@@ -77,51 +77,47 @@ namespace ChatBridge.Extensions.Telegram.Internal
                     if (!Directory.Exists("temp")) Directory.CreateDirectory("temp");
                     var filePath = $"temp/{fileInfo.FilePath.Split("/").Last()}";
 
-                    using (var fileStream = File.Create(filePath, 4096, FileOptions.DeleteOnClose))
+                    
+                    using (var fileStream = new MemoryStream())
                     {
                         await botClient.DownloadFileAsync(
                           fileInfo.FilePath,
                           fileStream
                         ); 
-
+                        
+                        fileStream.Position = 0L;
                         ISupportedImageFormat format = new PngFormat() { Quality = 70 };
-                        using (MemoryStream inStream = new MemoryStream())
+                        using (MemoryStream outStream = new MemoryStream())
                         {
-                            fileStream.Position = 0;
-                            await fileStream.CopyToAsync(inStream);
-                            using (MemoryStream outStream = new MemoryStream())
+                            // Initialize the ImageFactory using the overload to preserve EXIF metadata.
+                            using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
                             {
-                                // Initialize the ImageFactory using the overload to preserve EXIF metadata.
-                                using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
-                                {
-                                    //Size size = imageFactory.Load(inStream).Image.Size.Width == imageFactory.Load(inStream).Image.Size.Height ? new Size(150, 150) : imageFactory.Load(inStream).Image.Size;
-                                    Size size = new Size(150, 150);
-                                    // Load, resize, set the format and quality and save an image.
-                                    imageFactory.Load(inStream)
-                                        .Resize(size)
-                                        .Format(format)
-                                        .Save(outStream);
-                                }
-                                File.WriteAllBytes(filePath + ".png", outStream.GetBuffer());
-
-                                var apiClient = new ApiClient("");
-                                var httpClient = new HttpClient();
-                                var imageEndpoint = new ImageEndpoint(apiClient, httpClient);
-                                var imageUpload = await imageEndpoint.UploadImageAsync(outStream);
-                                var url = imageUpload.Link;
-                                receivedMessage = new BridgeMessage(_chat, senderName, new BridgeMessageContent[]
-                                {
-                                            new StickerContent(url)
-                                });
-
-                                Console.WriteLine();
-                                // Do something with the stream.
-                                //File.Delete(filePath + ".webp");
+                                Size size = new Size(150, 150);
+                                // Load, resize, set the format and quality and save an image.
+                                imageFactory.Load(fileStream)
+                                    .Resize(size)
+                                    .Format(format)
+                                    .Save(outStream);
                             }
-                        }
 
+                            var apiClient = new ApiClient("e85b52d0b7d494f");
+                            var httpClient = new HttpClient();
+                            var imageEndpoint = new ImageEndpoint(apiClient, httpClient);
+                            var imageUpload = await imageEndpoint.UploadImageAsync(outStream);
+                            var url = imageUpload.Link;
+                            receivedMessage = new BridgeMessage(_chat, senderName, new BridgeMessageContent[]
+                            {
+                                new StickerContent(url)
+                            });
+
+                            Console.WriteLine();
+                            // Do something with the stream.
+                            //File.Delete(filePath + ".webp");
+                        }
                     }
+
                    
+
                     //var fileId = update.Message.Sticker.FileId;
                     //var fileInfo = await botClient.GetFileAsync(fileId);
 
